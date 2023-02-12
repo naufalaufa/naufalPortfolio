0ec49220f154652891a18892451e861780ca545f
naufalaufa <muhammadnaufalaufarifqi@gmail.com>
2023-02-12 20:55:08 +0700

    initial commit

---
+++ JAVASCRIPT/js.js
@@ -0,0 +1,10 @@
+const ul = document.querySelector('nav ul')
+const hamburger = document.querySelector('.hamburger')
+hamburger.addEventListener('click' , function(){
+    ul.classList.toggle('slide')
+})
+
+const ula = document.querySelector('ul a')
+ula.addEventListener('click' , function(){
+    
+})
